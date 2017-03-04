f7a1d503595644a2987b4b2a17dd1bfb5cf54acf
1061696462 <1061696462@S108_PC16.fup.edu>
2017-03-03 21:05:18 -0500

    Se creo clase pelicula y unidad de persistencia

---
+++ src/main/java/co/edu/ims/modelo/Pelicula.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.ims.modelo;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Pelicula implements Serializable {
+    
+    // Atrinutos
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private List<Genero> genero;
+    private String nombre;
+
+    public Pelicula(Long id, List<Genero> genero, String nombre) {
+        this.id = id;
+        this.genero = genero;
+        this.nombre = nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Genero> getGenero() {
+        return genero;
+    }
+
+    public void setGenero(List<Genero> genero) {
+        this.genero = genero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+   
+}
